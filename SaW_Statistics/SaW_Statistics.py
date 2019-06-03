c64eaf6f4e71ff006120edc596ba8e8bfb643743
SandraWimberger <sandra.wimberger@gu.se>
2019-06-03 22:56:08 +0200

    Add pie plots for deletions and insertions

--- SaW_Statistics/SaW_Statistics.py
+++ SaW_Statistics/SaW_Statistics.py
@@ -34,21 +34,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import matplotlib.pyplot as plt
-editing = [float(wt_reads), float(editing_efficiency)]
-activities = ['wt_reads', 'mutated_reads']
-colors = ['r', 'g']
-plt.pie(editing, labels=activities, colors=colors, startangle=90, autopct='%.1f%%')
-plt.show()
+pie_editing_efficiency = plt.pie([float(wt_reads), float(editing_efficiency)], labels=['wt_reads', 'mutated_reads'], colors=['r', 'g'], startangle=90, autopct='%.1f%%')
 
 
 # Calculating number of reads with insertions
 insertions = sum(data[data['Type']== 'Insertion']['Count'])
 percent_insertions = insertions/mutated_reads*100
+pie_insertions = plt.pie([float(100 - percent_insertions), float(percent_insertions)], labels=['Others', 'Insertions'], colors=['r', 'g'], startangle=90, autopct='%.1f%%')
 
-print (percent_insertions)
 
 # Calculating number of reads with deletions
 deletions = sum(data[data['Type']== 'Deletion']['Count'])
 percent_deletions = deletions/mutated_reads*100
+pie_deletions = plt.pie([float(100 - percent_deletions), float(percent_insertions)], labels=['Others', 'Deletions'], colors=['r', 'g'], startangle=90, autopct='%.1f%%')
 
-print (percent_deletions)
