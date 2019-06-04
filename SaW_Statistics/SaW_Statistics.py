b0cd2f2fcff81a2c91f0fdf1ce75471ead9b8da5
SandraWimberger <sandra.wimberger@gu.se>
2019-06-04 09:43:35 +0200

    changed working directory back

--- SaW_Statistics/SaW_Statistics.py
+++ SaW_Statistics/SaW_Statistics.py
@@ -1,5 +1,5 @@
 import pandas as pd #Load the Pandas libraries with alias 'pd' 
-data = pd.read_csv("test_data_statistics.csv") # Read data from file 'filename.csv' [‎2019-‎05-‎29 09:21]  Taheri-Ghahfarokhi, Amir:  
+data = pd.read_csv('test_data_statistics.csv') # Read data from file 'filename.csv' [‎2019-‎05-‎29 09:21]  Taheri-Ghahfarokhi, Amir:  
 for i in range(10):
     var_type=data.loc[i,'Type']
     var_len=data.loc[i,'Length']
@@ -9,7 +9,7 @@ for i in range(10):
     duplication=data.loc[i,'Duplication']
     row= var_type + " " + str(var_len) + " " + allele
     print(row) 
- 
+
 print (data)
 #Type in the number of mapped reads from your tsv file
 mapped_reads = 50000
