beab348caa8f77d39d0811a6889fd2dc8b30715c
SandraWimberger <sandra.wimberger@gu.se>
2019-06-09 12:18:05 +0200

    Safing three fig as pdf on desktop

--- SaW_Statistics/SaW_Statistics.py
+++ SaW_Statistics/SaW_Statistics.py
@@ -33,38 +33,33 @@ print (wt_reads)
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 with PdfPages(r'C:\Users\kwcb136\Desktop\Charts.pdf') as export_pdf:
-    plt.figure(1)
+    fig1 = plt.figure('Percent editing eficiency')
     pie_editing_efficiency = plt.pie([float(wt_reads), float(editing_efficiency)], labels=['wt_reads', 'mutated_reads'], colors=['darkred', 'r'], startangle=90, autopct='%.1f%%')
     plt.title('Percent editing efficiency')
-    export_pdf.savefig()
-    plt.close()
-    
+    export_pdf.savefig(fig1)
     
     # Calculating number of reads with insertions
     insertions = sum(data[data['Type']== 'Insertion']['Count'])
     percent_insertions = insertions/mutated_reads*100
-    plt.figure(2)
+    fig2 = plt.figure('Percent insertions from modified reads')
     pie_insertions = plt.pie([float(100 - percent_insertions), float(percent_insertions)], labels=['Others', 'Insertions'], colors=['b', 'c'], startangle=90, autopct='%.1f%%')
     plt.title('Percent insertions from modified reads')
+    export_pdf.savefig(fig2)
     
     # Calculating number of reads with deletions
     deletions = sum(data[data['Type']== 'Deletion']['Count'])
     percent_deletions = deletions/mutated_reads*100
-    plt.figure(3)
+    fig3 = plt.figure('Percent deletions from modified reads')
     plt.pie([float(100 - percent_deletions), float(percent_insertions)], labels=['Others', 'Deletions'], colors=['g', 'y'], startangle=90, autopct='%.1f%%')
     plt.title('Percent deletions from modified reads')
+    export_pdf.savefig(fig3)
+    
+    plt.close()
     
     
-    
-    plt.show()
+   
 
 
     
     
     
-# Generating pdf with graphs
-def save_as_pdf(self):
-        from matplotlib.backends.backend_pdf import PdfPages
-        with PdfPages(os.path.join(self.path, self.name+str("test.pdf"))) as pdf:
-            pdf.savefig()
-            plt.close() 
