174f70fe313be1f4bb61f3bc89ab286d5ae0e04c
SongyuanLee <songyuan.li08@gmail.com>
2019-06-11 23:07:01 +0200

    Add files via upload

---
+++ Song_InDup/Duplication_v1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+Refseq=input("please enter the reference sequence below:\n")
+gRNA=input("please enter the gRNA sequence below:\n")
+
+import pandas
+df = pandas.read_csv('RIMA.csv', sep=';')
+df['Duplication1']=''
+count_row=df.shape[0]
+df['Duplication1']=''
+Refseq='TGCCTGCATTTTAGTCGTGAGATGGAGAATAAAGAAACTCTCAAAGGGTTGCACAAGATGGATGATCGTCCAGAGGAACGAATGATCAGGGAGAAACTGAAGGCAACCTGTATGCCAGCCTGGAAGCACGAATGGTTGGAAAGGAGAAATAGGCGAGGGCCTGTGGTAAGTGGCTATGGG'
+gRNA='AGCCTGGAAGCACGAATGGT'
+cut=Refseq.find(gRNA[0:16])+16
+print(cut,Refseq[cut])
+for i in range(0,count_row):
+    if df.loc[i]['Allele']!='-':
+        allele=df.loc[i]['Allele']
+        position=df.loc[i]['Reference Position']
+        len_all=len(allele)
+        if allele==Refseq[position-len_all:position] or allele==Refseq[position:position+len_all]:
+            if df.loc[i]['Type']=='Insertion':    
+                df.at[i,'Duplication1']='Duplication'
+                print(df.loc[i]['Allele'], df.loc[i]['Duplication'],df.loc[i]['Duplication1'])
+            else:
+                continue
+        else:
+            continue
+    else:    
+        continue
+df.to_csv('RIMA_Duplication.csv')
+   
+
