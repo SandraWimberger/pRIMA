3c3664b0e7f17863fca1db5310810d99a2efdd12
mocarl <calle.moller@gmail.com>
2019-06-12 18:58:24 +0200

    working micro_homo_panda

--- CM_deletions/micro_homology_pd.py
+++ CM_deletions/micro_homology_pd.py
@@ -8,19 +8,31 @@ Uses a "dataframe" format created by the pandas package
 """
 
 def micro_homo_pd(dataframe, refseq):
+    dataframe = dataframe.assign(DelCount=None) #Creates a new column and assigns it as None
     with open(refseq) as ref_seq_READ: #open reference sequence
         ref_seq=ref_seq_READ.read()
         print(ref_seq)
 
-    deletions = dataframe.loc[dataframe.loc[:, 'Type'] == "Deletion", ['Reference Position','Reference']] #extracts reference position and reference seq for all positions called as deletion
+    deletions = dataframe.loc[dataframe.loc[:, 'Type'] == "Deletion", ['Reference Position','Reference', 'DelCount']] #extracts reference position and reference seq for all positions called as deletion
     print(deletions)
     for idx, ref_pos in enumerate(deletions.loc[:,"Reference Position"]):
-        #print(idx, ref_pos)
-        for char in deletions.iloc[idx, 1]:
+        #print(ref_pos)
+        #print(type(ref_pos))
+
+        if len(deletions.iloc[idx, 1]) <= 1:
+            deletions.iloc[idx, 2] = None
+        else:
             count = 0
-            if char == ref_seq[ref_pos+len(deletions.iloc[idx, 1])-1]:
-                count +=1
-            else:
-                break
-        dataframe.
-         #   if char == ref_seq
+            for i, char in enumerate(deletions.iloc[idx, 1]):
+                print(char)
+                #print(deletions.iloc[idx, 1])
+
+                if char == ref_seq[ref_pos+len(deletions.iloc[idx, 1])-1+i]:
+                    count += 1
+                else:
+                    break
+            print(ref_seq)
+            print(deletions.iloc[idx, 1])
+            deletions.iloc[idx, 2] = count
+             #   if char == ref_seq
+    print(deletions)
