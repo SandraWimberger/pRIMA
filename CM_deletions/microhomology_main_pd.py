b6c8a3d6ac832f843dc782e96e0cfa05e7ff4cd6
mocarl <calle.moller@gmail.com>
2019-06-13 18:01:59 +0200

    mods

--- CM_deletions/microhomology_main_pd.py
+++ CM_deletions/microhomology_main_pd.py
@@ -8,8 +8,9 @@ test_data = csv_parser_pd("test_dataset.csv")
 
 from micro_homology_pd import micro_homo_pd
 
-data = micro_homo_pd(test_data, "test_data_refseq")
+data = micro_homo_pd(test_data, "REF_SEQ")
 print(data)
-#from csv_csv_pandas.csv_writer_pd import csv_writer_pd
 
-#test_data = csv_writer_pd(data, 'test_data_MH.csv')
+from csv_csv_pandas.csv_writer_pd import csv_writer_pd
+
+test_data = csv_writer_pd(data, 'test_data_MH.csv')
